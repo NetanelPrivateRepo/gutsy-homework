2f06843ab921c6eb079e6ebf8f8ebe3d0e703b52
NetanelPrivateRepo <88984990+NetanelPrivateRepo@users.noreply.github.com>
2025-03-02 10:15:38 +0200

    fix ingress issue

--- script.sh
+++ script.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
-# set minikube to work with local images
-eval $(minikube docker-env)
-
+#minikube setup
+eval $(minikube docker-env) # set minikube to work with local images
 minikube start
+minikube addons enable ingress # enable ingress in minikube
 
 # build docker images on minikube registry
-docker build -t redis-server:latest -f docker/Dockerfile-redis data
+docker build -t redis-server:latest -f docker/Dockerfile-redis .
 docker build -t go-server:latest -f docker/Dockerfile-go data
 
 docker images
 
+# Generate TLS certs
+openssl req -x509 -newkey rsa:4096 -keyout tls.key -out tls.crt -days 365 -nodes -subj "/CN=myapp.local"
+kubectl create secret tls my-tls-secret --cert=tls.crt --key=tls.key
+
+# Add minikube IP to local /etc/hosts file, using this seeting we could access our app from browser
+minikube_ip=$(minikube ip)
+#echo "$minikube_ip myapp.local" | sudo tee -a /etc/hosts
+echo "127.0.0.1 myapp.local" | sudo tee -a /etc/hosts
+
 # create redis / go-server deployment
 kubectl apply -f k8s/redis-deployment.yaml
 kubectl apply -f k8s/go-server-deployment.yaml
+
+
+
+sudo minikube tunnel
