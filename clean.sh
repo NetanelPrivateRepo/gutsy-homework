dea7af0e1ba0e789fc8dca3452f2b9e08cf96380
NetanelPrivateRepo <88984990+NetanelPrivateRepo@users.noreply.github.com>
2025-03-03 16:19:19 +0200

    add to clean script remove old conf files

--- clean.sh
+++ clean.sh
@@ -9,5 +9,9 @@ kubectl delete secret my-tls-secret
 # remove certs
 rm tls*
 
+# remove old conf files
+rm data/redis.conf.bak
+rm k8s/secret.yaml.bak
+
 # clean /etc/hosts file, remove the map for myapp.local
 sudo sed -i '' -e '$ d' /etc/hosts
